65bcc8eeacadaddcc3cce96af33a4985131cde4c
Radovan Sliz <sliz.radovan@gmail.com>
2022-10-22 15:19:16 +0200

    add - BE - ais id validation

--- backend/api/routers/core.py
+++ backend/api/routers/core.py
@@ -7,6 +7,13 @@ router = APIRouter()
 class BaseAisIdModel(BaseModel):
     id: int
 
+    @validator("id")
+    def ais_id_valid(cls, id):
+        if len(str(id)) >= 5 and len(str(id)) < 7:
+            return id
+        else:
+            raise ValueError("Provided AIS ID is not valid")
+
 
 
 @router.get("/health")
@@ -16,4 +23,12 @@ def healthcheck():
 
 @router.post("/start")
 async def start_simulator(aisId: BaseAisIdModel):
-    return aisId
+    try:
+        BaseAisIdModel(aisId)
+        return aisId
+    except Exception as e:
+        return {
+            "success": False,
+            "error": "Given AIS ID is not valid.",
+            "detail": str(e)
+        }

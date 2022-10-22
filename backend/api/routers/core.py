45dc7ff14b76f5fdbb64856ab73b60466dd381cf
Radovan Sliz <sliz.radovan@gmail.com>
2022-10-22 14:56:47 +0200

    Refactor - BE

--- backend/api/routers/core.py
+++ backend/api/routers/core.py
@@ -14,14 +14,6 @@ def healthcheck():
     return {"status": 200, "message": "Alive as never before."}
 
 
-
-@router.post("/initialize/{ais_id}")
-async def url_upload(ais_id: str):
-    return {"success": True, "id": ais_id}
-
-
 @router.post("/start")
 async def start_simulator(aisId: BaseAisIdModel):
-    print("FICI TO")
-    print(aisId)
     return aisId

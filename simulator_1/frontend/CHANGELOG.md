a061004e4bfcbfef687a06236ffc7027b5fec587
Radovan Sliz <sliz.radovan@gmail.com>
2023-01-08 15:05:44 +0100

    Remove old simulator

--- simulator_1/frontend/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-
-# 0.0.2
-### (??.??.20??)
-
-# 0.0.1
-- Project setup
-- Install packages
--- simulator_1/frontend/src/views/SupportView.vue
+++
@@ -1,9 +0,0 @@
-<template></template>
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-const goBack = () => {
-  router.go(-1)
-}
-</script>
